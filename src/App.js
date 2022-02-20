c755129307aa0cad4541773442a531ba97330f94
evsem <frukwork@gmail.com>
2022-02-20 23:40:25 +0300

    startProjectRedux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import './Style/App.css'
 
-function App() {
+const App = () => {
+  //Для изменения состояние нам нужен dispatch, создаём его через хук useDispatch
+  const dispatch = useDispatch()
+
+  //Для получения состояние нужно воспользоваться хуком useSelector, который параметром принимает функцию; сама функция принимает параметром состояние и из этого состояния мы получаем нужную переменную(смотри пример ниже)
+  const counter = useSelector((state) => state.counter)
+
+  const increment = () => {
+    dispatch({ type: 'INCREMENT', payload: 2 })
+  }
+  const decrement = () => {
+    dispatch({ type: 'DECREMENT', payload: 2 })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 className="App_titleCounter">{counter}</h3>
+      <div className="App_buttonsContainer">
+        <button className="App_btn" onClick={() => decrement()}>
+          Decrement
+        </button>
+        <button className="App_btn" onClick={() => increment()}>
+          Increment
+        </button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import App from './App'
+
+//Состояние(state) - это некторое значение в виде объекта, массива или примитивного значения, которе хранит в себе данные
+const defaultState = {
+  counter: 0,
+}
+
+//action - просто объект, состоящий из type и payload; по type мы определяем как состояние будет меняться; в payload мы передаём данные
+//let action = { type: '', payload: '?' }
+
+//Первым параметром данная функция принимает состояние(state), вторым - action(само действие)
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case 'DECREMENT':
+      return { ...state, counter: state.counter - action.payload }
+    case 'INCREMENT':
+      return { ...state, counter: state.counter + action.payload }
+    default:
+      return state
+  }
+}
+
+//Создаём store
+const store = createStore(reducer)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  //Оборачиваем компонент App в Provider для того чтобы связать Redux с React; в качестве параметра принимает вышк созданный store
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
